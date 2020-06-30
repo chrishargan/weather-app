1bc7234a17b84f080a803901ee4916fb1b774075
chrishargan <chrishargan@gmail.com>
2020-06-30 17:05:29 +0200

    tuesday commit

--- app.js
+++ app.js
@@ -18,16 +18,16 @@ window.addEventListener("load", () => {
                 })
                 .then(data => {
                     console.log(data);
-
                 })
+                .catch(err => alert("Error finding data"))
 
-        });
+        }
+        )
+    };
+ */
 
-});
 
-    }
-
-*/
+// on click search for city weather
 const forecast = document.getElementById("forecast");
 const addForecastPerDay = (list, count) => {
     let averageTempArray = [];
@@ -53,10 +53,13 @@ const addForecastPerDay = (list, count) => {
     })
 
     forecast.innerHTML += `
+    <div class ="daycast">
+
     ${new Date(list[count].dt_txt).toDateString()}
     Average Temp: ${Math.round(averageTemp * 10) / 10} °C
-    Lowest: ${Math.round(minTemp * 10)}°C
-    Highest: ${Math.round(maxTemp * 10)}°C
+    Lowest: ${Math.round(minTemp * 10) / 10}°C
+    Highest: ${Math.round(maxTemp * 10) / 10}°C
+    </div>
     `
 }
 
@@ -102,62 +105,3 @@ document.getElementById("search-button").addEventListener("click", () => {
             }
         })
 });
-
-
-
-
-
-
-
-
-
-/*
-
-
-document.getElementById("search-button").addEventListener("click", () => {
-    let city = document.getElementById("search-bar").value;
-    forecast.innerText
-    let where = document.getElementById("location-city");
-    let icon = document.getElementById("icon");
-    let description = document.getElementById("temerature-description");
-    let tempNow = document.getElementById("temp-Now");
-
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=b8b008d6f3645b497e4478b5f7ae8916`)
-        .then(response => {
-            return response.json()
-        })
-        .then(data => {
-
-            console.log(data.list)
-
-            for (let i = 0; i < 8; i++) {
-
-
-            }
-
-
-
-            //console.log(data.list[0].main.temp)
-            // while loop through the 5 days checking for the first day how many parts are in the first object as it depends on the time you check, the rest are 8 objects of 3hours each)
-
-
-            /*
-                        let locationValue = data["name"];
-                         let tempValue = data.list["list"][0]["main"]["temp"];
-                          let descriptionValue = data["weather"][0]["description"];
-                          let iconValue = data["weather"][0]["icon"];
-
-                          where.innerText = locationValue;
-                          description.innerText = descriptionValue;
-                          icon.innerText = iconValue;
-                          tempNow.innerText = tempValue;
-
-              */
-  //      });
-    //.catch(error => alert("Please enter City again"));
-
-
-
-    //   });
-
-
